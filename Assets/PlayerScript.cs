b67a9828d6bb0b4190b0ea645dbec5f4b47e18f3
Arttu Kangas <ttu91@hotmail.com>
2021-08-23 15:54:41 +0300

    PlayerScript

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float moveSpeed = 8f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    private Vector3 move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float zAxis = Input.GetAxis("Vertical");
+
+        move = transform.right * xAxis + transform.forward * zAxis;
+
+        controller.Move(move * moveSpeed * Time.deltaTime);
+    }
+}
